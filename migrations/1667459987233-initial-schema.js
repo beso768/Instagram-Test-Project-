be0517c56651e9249607453b7022fdfe7fef1e76
beso <besogigashvili818@gmail.com>
2022-11-03 11:34:00 -0700

    testing

--- migrations/1667459987233-initial-schema.js
+++ migrations/1667459987233-initial-schema.js
@@ -5,13 +5,13 @@ module.exports = class initialSchema1667459987233 {
 
   async up(queryRunner) {
     await queryRunner.query(
-      `CREATE TABLE "user" ("id" integer PRIMARY KEY  NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "username" varchar NOT NULL, "profile_pic_url" varchar NOT NULL)`,
+      `CREATE TABLE "user" ("id" integer PRIMARY KEY  AUTOINCREMENT  NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "username" varchar NOT NULL, "profile_pic_url" varchar NOT NULL)`,
     );
     await queryRunner.query(
-      `CREATE TABLE "tag" ("id" integer PRIMARY KEY  NOT NULL, "name" varchar NOT NULL, "media_count" integer NOT NULL DEFAULT (0), "userId" integer)`,
+      `CREATE TABLE "tag" ("id" integer PRIMARY KEY  AUTOINCREMENT  NOT NULL, "name" varchar NOT NULL, "media_count" integer NOT NULL DEFAULT (0), "userId" integer)`,
     );
     await queryRunner.query(
-      `CREATE TABLE "temporary_tag" ("id" integer PRIMARY KEY  NOT NULL, "name" varchar NOT NULL, "media_count" integer NOT NULL DEFAULT (0), "userId" integer, CONSTRAINT "FK_d0dc39ff83e384b4a097f47d3f5" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
+      `CREATE TABLE "temporary_tag" ("id" integer PRIMARY KEY  AUTOINCREMENT  NOT NULL, "name" varchar NOT NULL, "media_count" integer NOT NULL DEFAULT (0), "userId" integer, CONSTRAINT "FK_d0dc39ff83e384b4a097f47d3f5" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
     );
     await queryRunner.query(
       `INSERT INTO "temporary_tag"("id", "name", "media_count", "userId") SELECT "id", "name", "media_count", "userId" FROM "tag"`,
@@ -23,7 +23,7 @@ module.exports = class initialSchema1667459987233 {
   async down(queryRunner) {
     await queryRunner.query(`ALTER TABLE "tag" RENAME TO "temporary_tag"`);
     await queryRunner.query(
-      `CREATE TABLE "tag" ("id" integer PRIMARY KEY  NOT NULL, "name" varchar NOT NULL, "media_count" integer NOT NULL DEFAULT (0), "userId" integer)`,
+      `CREATE TABLE "tag" ("id" integer PRIMARY KEY  AUTOINCREMENT  NOT NULL, "name" varchar NOT NULL, "media_count" integer NOT NULL DEFAULT (0), "userId" integer)`,
     );
     await queryRunner.query(
       `INSERT INTO "tag"("id", "name", "media_count", "userId") SELECT "id", "name", "media_count", "userId" FROM "temporary_tag"`,
