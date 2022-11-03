6dfb55af929b65ce51eaf456285babbc5ad013e8
beso <besogigashvili818@gmail.com>
2022-11-03 12:08:07 -0700

    prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,14 +23,4 @@ import cookieSession from 'cookie-session';
 })
 export class AppModule {
   constructor(private configService: ConfigService) {}
-
-  // configure(consumer: MiddlewareConsumer) {
-  //   consumer
-  //     .apply(
-  //       cookieSession({
-  //         keys: [this.configService.get('COOKIE_KEY')],
-  //       }),
-  //     )
-  //     .forRoutes('*');
-  // }
 }
