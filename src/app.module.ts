1e7eb6484495ab0c08b9cd3d5e0de6b4f217d7df
beso <besogigashvili818@gmail.com>
2022-11-03 17:38:24 -0700

    credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,9 +15,10 @@ import { TagsModule } from './tags/tags.module';
       database: 'd3b45h24t01uhi',
       port: 5432,
       username: 'pcdoljvshzrotx',
+      ssl: false,
       password:
         '13cedea097fb079b4d7dea12bcbeb334288d19edc35d6f4dd5ebd45d7cb796f4',
-      url: 'postgres://pcdoljvshzrotx:13cedea097fb079b4d7dea12bcbeb334288d19edc35d6f4dd5ebd45d7cb796f4@ec2-23-23-151-191.compute-1.amazonaws.com:5432/d3b45h24t01uhi      ',
+      url: 'postgres://pcdoljvshzrotx:13cedea097fb079b4d7dea12bcbeb334288d19edc35d6f4dd5ebd45d7cb796f4@ec2-23-23-151-191.compute-1.amazonaws.com:5432/d3b45h24t01uhi',
       entities: [User, Tag],
       synchronize: true,
     }),
